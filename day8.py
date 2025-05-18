9046ebc4f48c30a069ef7eb34f7160dbbe370ac9
Erkinbek <erkinbekabdurakhmanov@gmail.com>
2025-05-17 20:48:33 -0400

    8th day

---
+++ day8.py
@@ -0,0 +1,19 @@
+def greeting():
+    print("Hi?")
+    print("Wassap bitch?")
+    print("How are you doing?")
+    
+
+def greet(name):
+    print(f" Hi {name}? ")
+    
+greet("Erik")
+
+def greet_with(person, location):
+    print(f'Hello {person}')
+    print(f"What is it like in {location}")
+    
+greet_with("Jack","Dallas")
+
+greet_with(person="Erkinbek", location="New-York")
+
